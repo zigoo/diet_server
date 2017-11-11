b013d6e6bcb91f892414a4b89ca9cedf2a8d3e22
zigoo <xzigoo@gmail.com>
2017-11-11 17:42:45 +0100

    xxx

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
-mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:auth/auth');
-mongoose.connect('mongodb://herok:herok123@ds145303.mlab.com:45303/diet_workshop');
+const fs = require('fs');
 
+
+var LiveChatApi = require('livechatapi').LiveChatApi;
+var api = new LiveChatApi('xzigoo@gmail.com', '58613798363e80f511ae3e3ce1aef5ab')
 
 
 app.use(morgan('combined'));
@@ -19,7 +19,39 @@ app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
+// const server = http.createServer(app);
+// server.listen(port);
 console.log('Server listening on:',port);
 
+api.status.get({ group: 0 }, function(data){
+  console.log(data);
+});
+
+http.createServer( function(req, res) {
+
+	// fs.readFile('./views/index.html', function (err, html) {
+	//     if (err) {
+	//         throw err; 
+	//     }       
+	//     http.createServer(function(request, response) {  
+	//         response.writeHeader(200, {"Content-Type": "text/html"});  
+	//         response.write(html);  
+	//         response.end();  
+	//     }).listen(80);
+	// });
+	fs.readFile('./views/index.html', function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			// if (mimeType != undefined) {
+				res.setHeader("Content-Type", "text/html");
+			// }
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+				console.log('no errors')
+			res.writeHead(500);
+			res.end();
+		}
+	});
+
+ }).listen(port);
--- router.js
+++ router.js
@@ -1,15 +1,17 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/', requireAuth, function(req,res) {
+  app.get('/', function(req,res) {
     res.send({ message: 'secret code aa1x' });
   });
 
-  app.post('/signin', requireSignin, Authentication.signin);
-  app.post('/signup', Authentication.signup);
+  // app.post('/signin', requireSignin, Authentication.signin);
+  // app.post('/signup', Authentication.signup);
 };
+
+
+// var LiveChatApi = require('livechatapi').LiveChatApi;
+// var api = new LiveChatApi('username@domain.com', 'APIKEY')
+// api.status.get({ group: 0 }, function(data){
+//   console.log(data);
+// });
